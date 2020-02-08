87893040f76d7298911d24bac82ed8551b0ab426
Hanna_Lata <annalats32@gmail.com>
2020-02-08 17:10:10 +0200

    Initial commit

---
+++ src/com/mainacad/AppRunner.java
@@ -0,0 +1,12 @@
+package com.mainacad;
+
+import java.util.logging.Logger;
+
+public class AppRunner {
+
+    private static final Logger LOGGER = Logger.getLogger(AppRunner.class.getName());
+
+    public static void main(String[] args) {
+
+    }
+}
