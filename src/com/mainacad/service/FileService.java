75569e0c59b5f29c7e91c3ede023e89495eab062
Hanna_Lata <annalats32@gmail.com>
2020-02-08 17:19:42 +0200

    Add functionality Java 1.7+

--- src/com/mainacad/service/FileService.java
+++ src/com/mainacad/service/FileService.java
@@ -1,46 +1,42 @@
 package com.mainacad.service;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class FileService {
 
     public static final String MAIN_DIR = System.getProperty("user.dir");
     public static final String SEP = System.getProperty("file.separator");
     public static final String FILES_DIR = MAIN_DIR + SEP + "files";
-
+    
     // work with text
     public  static void writeTextToFile(String text, String fileName) {
         checkTargetDir();
-
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(FILES_DIR + SEP + fileName);
+        try (FileWriter fileWriter = new FileWriter(FILES_DIR + SEP + fileName)) {
             fileWriter.write(text);
             fileWriter.flush();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
-
     }
-
     private static void checkTargetDir() {
         File file = new File(FILES_DIR);
         if (!file.exists()) {
             file.mkdir();
         }
     }
-
     public  static String readTextFromFile(String fileName) {
         String out = "";
 
+        try (FileReader fileReader = new FileReader(FILES_DIR + SEP + fileName);
+        BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line;
+            while ( (line = bufferedReader.readLine()) != null ) {
+                out += line + "\n";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return out;
     }
 }
