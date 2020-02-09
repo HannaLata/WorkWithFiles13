b14d26314880ec2986b437861003e6a59d6b6bc0
Hanna_Lata <annalats32@gmail.com>
2020-02-09 16:34:30 +0200

    Add and copy file

--- src/com/mainacad/AppRunner.java
+++ src/com/mainacad/AppRunner.java
@@ -1,5 +1,7 @@
 package com.mainacad;
 
+import com.mainacad.service.FileService;
+
 import java.util.logging.Logger;
 
 public class AppRunner {
@@ -7,6 +9,16 @@ public class AppRunner {
     private static final Logger LOGGER = Logger.getLogger(AppRunner.class.getName());
 
     public static void main(String[] args) {
+
+        String text = "Some new text\nI wanted to be written\n";
+
+        FileService.writeTextToFile(text, "test.txt", false);
+
+        String inputText = FileService.readTextFromFile("test.txt");
+
+        LOGGER.info("\n" + inputText);
+
+        FileService.copyFile("cat.jpg", "cat_copy.jpg");
 
     }
 }
--- src/com/mainacad/service/FileService.java
+++ src/com/mainacad/service/FileService.java
@@ -1,15 +1,16 @@
 package com.mainacad.service;
 
 import java.io.*;
+import java.nio.file.Files;
 
 public class FileService {
 
     public static final String MAIN_DIR = System.getProperty("user.dir");
     public static final String SEP = System.getProperty("file.separator");
     public static final String FILES_DIR = MAIN_DIR + SEP + "files";
-    
+
     // work with text
-    public  static void writeTextToFile(String text, String fileName) {
+    public  static void writeTextToFile(String text, String fileName, boolean append) {
         checkTargetDir();
         try (FileWriter fileWriter = new FileWriter(FILES_DIR + SEP + fileName)) {
             fileWriter.write(text);
@@ -38,5 +39,30 @@ public class FileService {
             e.printStackTrace();
         }
         return out;
+    }
+
+    public  static void writeBytesToFile(byte[] bytes, String fileName) {
+        checkTargetDir();
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILES_DIR + SEP + fileName)) {
+            fileOutputStream.write(bytes);
+            fileOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static byte[] getBytesFromFile ( String fileName ) {
+        File file = new File(FILES_DIR + SEP + fileName);
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    public static void copyFile ( String sourceName, String targetName ) {
+        byte[] bytes = getBytesFromFile(sourceName);
+        writeBytesToFile(bytes, targetName);
     }
 }
